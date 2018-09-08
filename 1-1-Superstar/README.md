9abea5a0679ead91f33fb9f9e83a6aa741ca1c76
itsLeonR <leon.rauschenbach@web.de>
2018-09-08 12:23:50 +0200

    Task description, first working Algorithm for Superstar.

---
+++ 1-1-Superstar/README.md
@@ -0,0 +1,25 @@
+# Superstar
+
+
+## Implementation
+
+
+## Aufgabe
+Im sozialen Netzwerk TeeniGram können Mitglieder anderen Mitgliedern „folgen“. Natürlich können sich zwei Personen gegenseitig folgen, was auch oft vorkommt. 
+Eine Spezialität von TeeniGram sind die Gruppen mit ihrer Superstar-Funktion. Ein Mitglied X einer Gruppe heißt Superstar
+der Gruppe, wenn alle anderen Mitglieder der Gruppe dem Mitglied X folgen, aber X selbst keinem anderen Mitglied der Gruppe folgt. Ein Beispiel: Eine kleine Gruppe hat drei Mitglieder, nämlich Selena, Justin und Hailey. Selena folgt Justin, Hailey folgt Justin und Hailey folgt Selena. Justin ist der Superstar.
+Werbetreibende wollen sehr gerne wissen, wer in einer Gruppe der Superstar ist. In TeeniGram ist es aber nicht so einfach, an Informationen heranzukommen. Nur Anfragen der Form „Folgt Mitglied Y Mitglied Z ?“ sind erlaubt, und jede Anfrage kostet einen ganzen Euro. Deshalb sollen möglichst wenige Anfragen gestellt werden.
+
+Aufgabe 1 
+Überlege dir ein Verfahren, das nur mit Hilfe solcher Anfragen herausfindet, ob es unter den 
+Mitgliedern einer Gruppe einen Superstar gibt. Falls die Antwort „ja“ lautet, soll der Superstar
+namentlich ausgegeben werden. 
+Versuche, die Anzahl der gestellten Anfragen klein zu halten. Kannst du die maximal 
+notwendige Anzahl der Anfragen deines Verfahrens relativ zur Anzahl der Mitglieder der 
+Gruppe abschätzen?
+Schreibe ein Programm, das Gruppen einliest, dein Verfahren ausführt und die gestellten 
+Anfragen samt den Antworten ausgibt. Falls ein Superstar gefunden wird, soll dieser benannt 
+werden.
+Wende dein Programm auf die Beispiele an, die du auf den 
+BWINF
+-Webseiten findest
--- 1-1-Superstar/main.go
+++ 1-1-Superstar/main.go
@@ -6,34 +6,84 @@ import (
 
 // Basic Structure to define an user
 type user struct {
-	name, group string
-	followers   []string
-	following   []string
+	name      string
+	followers []string
+	following []string
 }
+
+// Counter to know how much Requests we are making
+var isFollowingCounter int
 
 func main() {
 
-	// ====== Define some sample Data =========
-	justin := user{
-		name:      "Justin",
-		group:     "Kids",
-		followers: []string{"Hailey", "Selena"},
+	// ====== Define a sample Group with Users in it =========
+	var samplegroup = []user{
+		user{
+			name:      "Selena",
+			followers: []string{"Hailey"},
+			following: []string{"Justin"},
+		},
+		user{
+			name:      "Justin",
+			followers: []string{"Hailey", "Selena"},
+		},
+		user{
+			name:      "Hailey",
+			following: []string{"Justin", "Selena"},
+		},
 	}
+	// Check if theres a Superstar in group[]
+	getSuperstar(samplegroup)
+}
 
-	selena := user{
-		name:      "Selena",
-		group:     "Kids",
-		followers: []string{"Hailey"},
-		following: []string{"Justin"},
+func getSuperstar(searchgroup []user) {
+	for index := 0; index < len(searchgroup); index++ {
+		// fmt.Println("Testing this Group: ", searchgroup)
+		isUserSuperstar(index, searchgroup)
 	}
+	fmt.Println("We used", isFollowingCounter, "Requests")
+}
 
-	hailey := user{
-		name:      "Hailey",
-		group:     "Kids",
-		following: []string{"Justin", "Selena"},
+func isUserSuperstar(userindex int, tmpGroup []user) {
+	testUser := tmpGroup[userindex]
+	fmt.Println("Testing:", testUser.name)
+	// Deletes testUser from tmpGroup
+	testGroup := removeUserFromGroup(userindex, tmpGroup)
+
+	// User cant be an Superstar if hes following someone in the Group
+	for index := 0; index < len(testGroup); index++ {
+		if isFollowing(testUser, testGroup[index]) {
+			fmt.Println(testUser.name, "is following someone in the group, he's/she's not a Superstar")
+			return // Abort
+		}
 	}
-	// Print out Sample Data
-	fmt.Println(justin)
-	fmt.Println(selena)
-	fmt.Println(hailey)
+	// Test if all Users in the Group are following testUser
+	for index := 0; index < len(testGroup); index++ {
+		if !isFollowing(testGroup[index], testUser) {
+			fmt.Println("Not all Users are following", testUser.name, "he's/she's not a Superstar")
+			return
+		}
+	}
+	// If these two pass we have a Superstar
+	fmt.Println(testUser.name, "is an SuperStar")
+}
+
+func isFollowing(person1, person2 user) bool {
+	// Increment the FollowingCounter
+	isFollowingCounter++
+	// Iterate over the followers of person2 and check if person1's name is in there
+	for _, follower := range person2.followers {
+		if follower == person1.name {
+			// We have a match
+			return true
+		}
+	}
+	return false
+}
+
+func removeUserFromGroup(userindex int, tmpGroup []user) []user {
+	removedGroup := make([]user, len(tmpGroup))
+	copy(removedGroup, tmpGroup)
+	removedGroup = append(removedGroup[:userindex], removedGroup[userindex+1:]...)
+	return removedGroup
 }
